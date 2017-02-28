343eb76b8c4fafb6c20a0aa1db8740f67e72dc7d
rhsiao <richard8792@gmail.com>
2017-02-28 14:05:34 -0800

    Changed values to integers

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function readClick(id) {
         //firebaseRef.set("") //Sets value to firebase. Can't set to main object, so need to set a key to add a child to that
     firebaseRef.child(id).once('value', function(datasnapshot) {
             if (datasnapshot.val() == null) { //Since the webpage starts cleared on load of data. This just assigns a value on the first click of the button.
-                firebaseRef.child(id).set("1");
-            } else if (datasnapshot.val() !== '0') {
-                firebaseRef.child(id).set("0")
+                firebaseRef.child(id).set(1);
+            } else if (datasnapshot.val() !== 0) {
+                firebaseRef.child(id).set(0)
             } else {
-                firebaseRef.child(id).set("1")
+                firebaseRef.child(id).set(1)
             }
 
         }) //console.log(datasnapshot.val()) Console logs the value in the id, for debugging
